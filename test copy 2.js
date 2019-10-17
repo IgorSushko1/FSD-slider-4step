4037ea584aadba11e7ed30baa736a0a88efa2396
Igor Sushko <igor_sushko@yahoo.com>
2019-10-17 15:53:10 +0500

    Изменения в слайдере:
    Сделано приятное оформление
    
    Сделана шкала,которая следует за ползунками, если их два
    
    Сделан вывод значений над ползунками и в отдельное поле

--- test copy 2.js
+++ test copy 2.js
@@ -1,5 +1,42 @@
 let elements = document.querySelectorAll(".drag");
 
+let objModel = {
+	sign: "$",
+	minLimit: 0,
+	maxLimit: 300,
+	slider_Start_Position: 100,
+	slider_End_Position: 200
+}
+
+function mathSlider(give_parentWidth, sliderElement1Position, sliderElement2Position) {
+	// give_parentWidth - ширина поля слайдера
+	// parentPositionX  - отступ поля слайдера от края экрана по оси X
+	// sliderElement1Position - позиция слайдера №1
+	// sliderElement2Position - позиция слайдера №2
+
+	// необходимо реализовать перемещение поля вслед за ползунком
+
+	let minLimit = objModel.minLimit;
+	let maxLimit = objModel.maxLimit;
+	let parentWidth = give_parentWidth;
+	let calculate = (maxLimit - minLimit) / parentWidth;
+
+	let value1 = document.getElementById("drag2_value");
+	let value2 = document.getElementById("drag3_value");
+
+	value1.style.left = sliderElement1Position+7.5 + "px";
+	value2.style.left = sliderElement2Position+7.5 + "px";
+
+	value1.innerText = Math.floor((sliderElement1Position + 15) * calculate);
+	value2.innerText = Math.ceil((sliderElement2Position + 15) * calculate);
+
+	let value1Field = document.getElementById("value1-field");
+	let value2Field = document.getElementById("value2-field");
+
+	value1Field.innerText = Math.floor((sliderElement1Position + 15) * calculate) + " " + objModel.sign;
+	value2Field.innerText = Math.ceil((sliderElement2Position + 15) * calculate) + " " + objModel.sign;
+}
+
 if (elements.length == 2) {
 
 	dragElements(document.getElementById("drag2"), document.getElementById("drag3"))
@@ -34,7 +71,7 @@ function closeDragElement() {
 function elementDrag(e) {
 	let parentElement = document.getElementById("ias-slider_parent");
 	var parentPosition = parentElement.getBoundingClientRect();
-	let parentPositionX = parentPosition.left + pageXOffset;
+	let parentPositionX = parentPosition.left;
 	let parentWidth = parentElement.offsetWidth;
 
 	let sliderElement = document.getElementById("drag2");
@@ -77,28 +114,33 @@ function dragMouseDown2(e) {
 function elementDrag1(e) {
 	let parentElement = document.getElementById("ias-slider_parent");
 	var parentPosition = parentElement.getBoundingClientRect();
-	let parentPositionX = parentPosition.left + pageXOffset;
+	let parentPositionX = parentPosition.left;
 	let parentWidth = parentElement.offsetWidth;
 
 	let sliderElement1 = document.getElementById("drag2");
 	let sliderElement1Position = sliderElement1.offsetLeft; // вычисляет верхний левый угол элемента от угла родителя
-	console.log("sliderElement1Position : " + sliderElement1Position + "pageXOffset : " + pageXOffset);
+	console.log("sliderElement1Position : " + sliderElement1Position + " pageXOffset : " + pageXOffset);
 
 	let sliderElement2 = document.getElementById("drag3");
 	let sliderElement2Position = sliderElement2.offsetLeft; // вычисляет верхний левый угол элемента от угла родителя
-	console.log("sliderElement2Position : " + sliderElement2Position + "pageXOffset : " + pageXOffset);
+	console.log("sliderElement2Position : " + sliderElement2Position + " pageXOffset : " + pageXOffset);
+
+	ribon(sliderElement1Position, sliderElement2);
+	mathSlider(parentWidth, sliderElement1Position, sliderElement2Position);
 
 	if (e.clientX > parentWidth + parentPosition.left) {
 		e.clientX = parentPosition.left + parentWidth;
 		sliderElement1.style.left = (parentWidth - 15) + "px"
+
 	} else if (e.clientX < parentPosition.left) {
-		e.clientX = parentPosition.left;
+		e.clientX = parentPosition.leftt;
 		sliderElement1.style.left = "-15px"
+
 	} else {
 		sliderElement1.style.left = (e.clientX - parentPositionX - 15) + "px";
 	};
 
-	if (sliderElement2Position+parentPositionX < e.clientX+35) {
+	if (sliderElement2Position + parentPositionX < e.clientX + 15) {
 		e.clientX = sliderElement2Position + "px";
 		sliderElement1.style.left = (sliderElement2Position - 30) + "px"
 	}
@@ -106,30 +148,44 @@ function elementDrag1(e) {
 
 function elementDrag2(e) {
 	let parentElement = document.getElementById("ias-slider_parent");
-	var parentPosition = parentElement.getBoundingClientRect();
-	let parentPositionX = parentPosition.left + pageXOffset;
+	let parentPosition = parentElement.getBoundingClientRect();
+	let parentPositionX = parentPosition.left;
 	let parentWidth = parentElement.offsetWidth;
 
 	let sliderElement1 = document.getElementById("drag2");
 	let sliderElement1Position = sliderElement1.offsetLeft; // вычисляет верхний левый угол элемента от угла родителя
-	console.log("sliderElement1Position : " + sliderElement1Position + "pageXOffset : " + pageXOffset);
+	console.log("sliderElement1Position : " + sliderElement1Position + " pageXOffset : " + pageXOffset);
 
 	let sliderElement2 = document.getElementById("drag3");
 	let sliderElement2Position = sliderElement2.offsetLeft; // вычисляет верхний левый угол элемента от угла родителя
-	console.log("sliderElement2Position : " + sliderElement2Position + "pageXOffset : " + pageXOffset);
+	console.log("sliderElement2Position : " + sliderElement2Position + " pageXOffset : " + pageXOffset);
+
+	ribon(sliderElement1Position, sliderElement2);
+	mathSlider(parentWidth, sliderElement1Position, sliderElement2Position);
 
 	if (e.clientX > parentWidth + parentPosition.left) {
 		e.clientX = parentPosition.left + parentWidth;
 		sliderElement2.style.left = (parentWidth - 15) + "px"
+
 	} else if (e.clientX < parentPosition.left) {
 		e.clientX = parentPosition.left;
 		sliderElement2.style.left = "-15px"
+
 	} else {
 		sliderElement2.style.left = (e.clientX - parentPositionX - 15) + "px";
 	};
 
-	if (sliderElement1Position+parentPositionX > e.clientX-35) {
+	if (sliderElement1Position + parentPositionX > e.clientX - 45) {
 		e.clientX = sliderElement1Position + "px";
 		sliderElement2.style.left = (sliderElement1Position + 30) + "px"
 	}
+}
+
+function ribon(sliderElement1Position, sliderElement2) {
+
+	let ribon = document.getElementById("colorBar");
+	ribon.style.left = 15 + sliderElement1Position + "px";
+	ribon.style.width = sliderElement2.offsetLeft - sliderElement1Position + "px";
+
+	// ribon.style.width = valueMouseMoveArea + "px";
 }
