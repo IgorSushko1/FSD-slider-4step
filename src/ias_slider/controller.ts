048e3b045da00309ae74b04033aee79415e88f94
Igor Sushko <igor_sushko@yahoo.com>
2019-10-23 01:13:10 +0500

    Заполнение методами

--- src/ias_slider/controller.ts
+++ src/ias_slider/controller.ts
@@ -1,53 +1,87 @@
-interface Controller {
+import { View } from "./view";
 
+interface Controller {
+model: any;
+view: any;
+min_slider_value : number;
+max_slider_value : number;
 }
 
 class Controller {
-	constructor () {
+	constructor(view, model) {
+		this.view = view;
+		this.model = model;
+		this.min_slider_value;
+		this.max_slider_value;
 	}
 
-	create__slider(view, model) {
-		view.create();
-		model.inizializate();
-		this.check_param_in_model();
+	facade_set_from_view(obj) {
+		// for (let a in obj) {
+		// 	if (a in this) {
+		Object.assign(this, obj);
+		let refresh_model = this.obj_to_model();
+		this.facade_set_model(refresh_model);
+			// }
+		// }
+	}
+	obj_to_model() {
+		let obj_to_model = {
+			min_slider_value : this.min_slider_value,
+			max_slider_value : this.max_slider_value
+		}
+		return obj_to_model;
 	}
 
-	check_param_in_model = () => {//проверяет соответствие значений в модели и значений во вью
-		let model = this.facade_model(); // получил данные из модели
-		let model_keys = Object.keys(model); // получил ключи  из обхекта
-		
-		let view = this.facade_view; // получил данные из интерфейса
-		
-		for (let key in view) {
-		 // сравнил значения в модели и во вью
-			let a = view.hasOwnProperty(key)
-			if (view.a && view.key != model.key) {
-				this.status = "refresh" // если хоть одно не совпало то обновляются все данные в моделе и перестраивается вью
-			} else {
-				this.status = "okey" // если все совпало то ничего не перестраивается
-			}
-			}
-		}
-
-		refresh = () => {
-			// вызывается метод майн функции, который перестраивает вью сообщая ему новые данные.
-			// и снова идет проверка 
-			this.check_param_in_model;
-		}
-	
-
-	facade_model = () => {
-		return {
-			// возвращает массив значений из модели
-		}
+	facade_set_model(obj) {
+		this.model.set_model(obj);
 	}
 
-	facade_view = () => {
-		return {
-			// возвращает массив значений из вью
-		}
+	initialize() {
+		this.create_slider()
 	}
+
+	create_slider() {
+		this.view.create_stuff()
+	}
+
+	// check_param_in_model = () => {//проверяет соответствие значений в модели и значений во вью
+	// 	let model = this.facade_model(); // получил данные из модели
+	// 	let model_keys = Object.keys(model); // получил ключи  из обхекта
+
+	// 	let view = this.facade_view; // получил данные из интерфейса
+
+	// 	for (let key in view) {
+	// 		// сравнил значения в модели и во вью
+	// 		let a = view.hasOwnProperty(key)
+	// 		if (view.a && view.key != model.key) {
+	// 			this.status = "refresh" // если хоть одно не совпало то обновляются все данные в моделе и перестраивается вью
+	// 		} else {
+	// 			this.status = "okey" // если все совпало то ничего не перестраивается
+	// 		}
+	// 	}
+	// }
+
+	// refresh = () => {
+	// 	// вызывается метод майн функции, который перестраивает вью сообщая ему новые данные.
+	// 	// и снова идет проверка 
+	// 	this.check_param_in_model;
+	// }
+
+	// facade_model = () => {
+	// 	return {
+	// 		// возвращает массив значений из модели
+	// 	}
+	// }
+
+	// facade_view = () => {
+	// 	return {
+	// 		// возвращает массив значений из вью
+	// 	}
+	// }
 
 }
 
-let controller = new Controller(View, Model);
+let controller = new Controller({ view: new View() })
+
+// let controller = new Controller(View, Model);
+export { Controller }
