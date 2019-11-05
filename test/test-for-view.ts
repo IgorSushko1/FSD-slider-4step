dd20dc2b22f540ffe76c70c02c45be277804bf16
Igor Sushko <igor_sushko@yahoo.com>
2019-11-05 22:15:22 +0500

    1.1 тест родительского элемента

--- test/test-for-view.ts
+++ test/test-for-view.ts
@@ -1,18 +1,36 @@
-import { View } from '';
-import { IasSlider } from '';
+import { View_horizontal } from '../src/ias_slider v2/view_horizontal';
+// import { IasSlider } from '';
 import { expect, assert } from 'chai';
-import 'mocha';
-
+import * as mocha from 'mocha';
+// mocha.setup('bdd'); // используется если запускается в браузере
 describe('Тестирование View',
 	() => {
 		it('1. Наличие класса View', () => {
-			const result = new View();
+			let settings: any = {
+				_element_id: "id",
+				_sign: "₽",
+				_min_start_slider: 0,
+				_max_start_slider: 1000,
+				_min_slider_value: 200,
+				_max_slider_value: 800,
+				_slider_type: "duble",
+				_type_view: "horizontal",
+				_step: 50,
+				tooltip: "on",
+				value_field: "on",
+				settings_id: "none"
+			};
+			const result = new View_horizontal(settings);
 			expect(typeof result).to.equal("object");
+		});
+		it('2. Вызов родительского элемента', () => {
+			let parent = "<div id='ias_slider'></div>";
+			expect(View_horizontal.get_parent()).to.equal(parent);
 		})
-		it('2. Наличие в классе создания шкалы', () => {
-			const result = new View();
-			expect(result.createViewPresenter()).to.equal(true);
-		})
+		// it('2. Наличие в классе создания шкалы', () => {
+		// 	const result = new View();
+		// 	expect(result.createViewPresenter()).to.equal(true);
+		// })
 		// it('2. Наличие в классе создания шкалы', () => {
 		// 	const result = new ViewFasade();
 		// 	expect(result.refreshNumbers()).to.equal("success");
@@ -20,11 +38,13 @@ describe('Тестирование View',
 	})
 
 
-	describe('Тестирование CreateSlider',
-	() => {
-		it('1. Наличие класса IasSlider', () => {
-			const result = new IasSlider();
-			expect(result.createSlider()).to.equal("success");
-		})
-		
-	})
+// describe('Тестирование CreateSlider',
+	// () => {
+	// 	it('1. Наличие класса IasSlider', () => {
+	// 		const result = new IasSlider();
+	// 		expect(result.createSlider()).to.equal("success");
+	// 	})
+
+	// }
+// )
+// mocha.run(); // используется если запускается в браузере
