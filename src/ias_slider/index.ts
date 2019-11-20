cc9ba6b51c5c0806309af7977dbd8a3ebd931fd0
Igor Sushko <igor_sushko@yahoo.com>
2019-11-21 00:15:53 +0500

    is_slider now get object

--- src/ias_slider/index.ts
+++ src/ias_slider/index.ts
@@ -1,5 +1,5 @@
 import "../is_horizontal.css";
 import "../is_vertical.css";
 import {is_slider} from "./is_slider";
-is_slider("iss", "doc_panel");
-is_slider("iss_2", "doc_panel_2");
+is_slider({_element_id: "iss", settings_id: "doc_panel"});
+is_slider({_element_id:"iss_2", settings_id: "doc_panel_2"});
