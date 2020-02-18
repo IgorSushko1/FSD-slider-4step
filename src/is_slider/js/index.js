a19489b3372bd39001278c855112f2af44325c6a
Igor Sushko <igor_sushko@yahoo.com>
2020-02-18 15:27:15 +0500

    Файлы предыдущего слайдера Удалены

--- src/is_slider/js/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("../is_horizontal.css");
-require("../is_vertical.css");
-var is_slider_1 = require("./is_slider");
-is_slider_1.is_slider({ _element_id: "iss", settings_id: "doc_panel" });
-is_slider_1.is_slider({ _element_id: "iss_2", settings_id: "doc_panel_2" });
